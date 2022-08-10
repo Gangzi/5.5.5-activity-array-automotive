236329a0b6e41219280311a3ded771f1161fa1ce
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-08-09 23:08:13 -0400

    Automotive Ass

---
+++ vehiclebaseClass.js
@@ -0,0 +1,31 @@
+class Vehicle {
+    constructor(make, model, year, color, mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 0;
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start!");
+        }
+    }
+
+
+
+}
+
+
+
+
+
